a7f2354611cf2d47160f01fba6b1f8eb34c128a3
Seth Fiawoo <seth.fiawoo@shopify.com>
2024-04-05 09:14:00 -0600

    Add cookies to request

--- app/routes/wallets.checkouts[.json].tsx
+++ app/routes/wallets.checkouts[.json].tsx
@@ -34,41 +34,48 @@ export async function action({request, context}: LoaderFunctionArgs) {
     'X-Shopify-Client-IP-Sig':
       request.headers.get('X-Shopify-Client-IP-Sig') || '',
     'User-Agent': 'Hydrogen',
+    Cookie: request.headers.get('Cookie') || '',
   });
 
+  const body = await request.json();
+//   console.log('url', JSON.stringify(body.checkout));
   const response = await fetch(url + pathname + search, {
-    headers: customHeaders,
+    method: request.method,
+    headers: request.headers,
+    body: JSON.stringify(body),
   });
 
-  const data = await response.text();
+  const data = await response.json();
 
-  const processedData = data
-    .replace(
-      /<meta.*name="robots".*content="noindex.*".*>|<link.*rel="canonical".*href=".*".*>|("monorailRegion":"shop_domain")|<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi,
-      (match) => {
-        if (match.startsWith('<meta') && config.removeNoIndex) return '';
-        if (match.startsWith('<link') && config.updateCanonical)
-          return match.replace(url, origin);
-        if (match.startsWith('"monorailRegion"'))
-          return '"monorailRegion":"global"';
-        if (match.startsWith('<script') && config.ignoreRedirects)
-          return match.replace(/window\.location\.replace\([^)]*\);?/g, '');
-        return match;
-      },
-    )
-    .replace(new RegExp(url, 'g'), origin);
+  //   const processedData = data
+  //     .replace(
+  //       /<meta.*name="robots".*content="noindex.*".*>|<link.*rel="canonical".*href=".*".*>|("monorailRegion":"shop_domain")|<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi,
+  //       (match) => {
+  //         if (match.startsWith('<meta') && config.removeNoIndex) return '';
+  //         if (match.startsWith('<link') && config.updateCanonical)
+  //           return match.replace(url, origin);
+  //         if (match.startsWith('"monorailRegion"'))
+  //           return '"monorailRegion":"global"';
+  //         if (match.startsWith('<script') && config.ignoreRedirects)
+  //           return match.replace(/window\.location\.replace\([^)]*\);?/g, '');
+  //         return match;
+  //       },
+  //     )
+  //     .replace(new RegExp(url, 'g'), origin);
 
-  const status = /<title>(.|\n)*404 Not Found(.|\n)*<\/title>/i.test(data)
-    ? 404
-    : response.status;
+  //   const status = /<title>(.|\n)*404 Not Found(.|\n)*<\/title>/i.test(data)
+  //     ? 404
+  //     : response.status;
 
-  const headers = new Headers(response.headers);
-  headers.set('content-type', 'text/html');
-  headers.delete('content-encoding');
-  headers.set('Cache-Control', config.cacheControl);
+//   const headers = new Headers(response.headers);
+//   headers.set('Cache-Control', config.cacheControl);
+//   console.log('processedData', response.headers.get('set-cookie'));
 
-  return redirect('checkout.headlesseth.net/wallets/checkouts.json', {
-    status,
-    headers,
+  //   return new Response(data, {status: 201, headers});
+
+  return redirect(`${data.checkout.web_url}`, {
+    headers: {
+      'Set-Cookie': response.headers.get('set-cookie') || '',
+    },
   });
 }
